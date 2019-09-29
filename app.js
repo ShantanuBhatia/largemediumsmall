9afa5c2117ac6fb40fda06403fed97890fccd514
ShantanuBhatia <sb5455@nyu.edu>
2019-09-29 22:18:42 +0530

    MVP - paste URL to get text-only version of article

--- app.js
+++ app.js
@@ -1,32 +1,66 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 let app = express();
 
+app.set('view engine', 'ejs'); // for rendering templates
+app.use(bodyParser.urlencoded());
+
 const options = {
-	uri: `https://onezero.medium.com/heres-how-amazon-alexa-will-recognize-when-you-re-frustrated-a9e31751daf7`,
+	// uri: `https://onezero.medium.com/heres-how-amazon-alexa-will-recognize-when-you-re-frustrated-a9e31751daf7`,
+  uri: `https://medium.com/@cogdog/medium-your-rss-feeds-are-mess-ebfe6f731c22`,
 	transform: function(body){
 		return cheerio.load(body);
 	}
-};
+}; // for debugging
 
-
-const getArticle = () => {
-  // rp(options)
-  // .then(($) => {
-  //   console.log($);
-  //   return JSON.stringify($);
-  // })
-  // .catch((err) => {
-  //   return(err);
-  // });
-  return "Fuck, gimme SOMETHING";
+// For generating the HTML of the article
+const wrappedTag = (dom_ele) => {
+  return `<${cheerio(dom_ele).prop("tagName")}>${cheerio(dom_ele).html()}</${cheerio(dom_ele).prop("tagName")}>`;
 }
 
-app.get('/', function (req, res) {
-  console.log("GET on index");
-  res.send(getArticle());
+
+/*
+#############################################
+                VIEEEEEEEEEWS                
+#############################################
+*/
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.get('/tester', (req, res) => {
+  
+  rp(options)
+  .then(($) => {
+    article_content = "";
+    $('article').find('p, h1, h2').each((e,v)=> {article_content += wrappedTag(v);});
+    res.render('article', {art_content: article_content});
+  })
+  .catch((err) => {
+    return(err);
+  });
+});
+
+app.post('/getarticle', (req, res) => {
+  rp({
+    // uri: `https://onezero.medium.com/heres-how-amazon-alexa-will-recognize-when-you-re-frustrated-a9e31751daf7`,
+    uri: req.body.art_url,
+    transform: function(body){
+      return cheerio.load(body);
+    }
+  }).then(($) => {
+    article_content = "";
+    $('article').find('p, h1, h2').each((e,v)=> {article_content += wrappedTag(v);});
+    res.render('article', {art_content: article_content});
+  })
+  .catch((err) => {
+    return(err);
+  });
 });
 
 app.listen(3000, function () {
