544409b038e63fdf6bf8b98c9df733069e7776b4
ShantanuBhatia <sb5455@nyu.edu>
2019-10-02 19:51:27 +0530

    make it better

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/*
+################################################
+                DEPENDENCIEEEEES
+################################################
+*/
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const express = require('express');
@@ -8,47 +13,36 @@ let app = express();
 app.set('view engine', 'ejs'); // for rendering templates
 app.use(bodyParser.urlencoded());
 
-const options = {
-	// uri: `https://onezero.medium.com/heres-how-amazon-alexa-will-recognize-when-you-re-frustrated-a9e31751daf7`,
-  uri: `https://medium.com/@cogdog/medium-your-rss-feeds-are-mess-ebfe6f731c22`,
-	transform: function(body){
-		return cheerio.load(body);
-	}
-}; // for debugging
+
+/*
+################################################
+                BUSINESS LOGIIIC
+################################################
+*/
 
 // For generating the HTML of the article
 const wrappedTag = (dom_ele) => {
+
+  cheerio(dom_ele).each(() => {
+    cheerio(this).removeClass();
+  });
+
   return `<${cheerio(dom_ele).prop("tagName")}>${cheerio(dom_ele).html()}</${cheerio(dom_ele).prop("tagName")}>`;
 }
 
-
 /*
-#############################################
-                VIEEEEEEEEEWS                
-#############################################
+################################################
+                VIEEEEEEEEEEEEWS
+################################################
 */
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-
-app.get('/tester', (req, res) => {
-  
-  rp(options)
-  .then(($) => {
-    article_content = "";
-    $('article').find('p, h1, h2').each((e,v)=> {article_content += wrappedTag(v);});
-    res.render('article', {art_content: article_content});
-  })
-  .catch((err) => {
-    return(err);
-  });
-});
-
 app.post('/getarticle', (req, res) => {
   rp({
-    // uri: `https://onezero.medium.com/heres-how-amazon-alexa-will-recognize-when-you-re-frustrated-a9e31751daf7`,
+    // uri: `https://onezero.medium.com/heres-how-amazon-alexa-will-recognize-when-you-re-frustrated-a9e31751daf7`, - testing
     uri: req.body.art_url,
     pool: false,
     agent: false,
@@ -57,13 +51,21 @@ app.post('/getarticle', (req, res) => {
     }
   }).then(($) => {
     article_content = "";
-    $('article').find('p, h1, h2').each((e,v)=> {article_content += wrappedTag(v);});
+    $('article').find('p, h1, h2, figure, pre').each((e,v)=> {article_content += wrappedTag(v);});
     res.render('article', {art_content: article_content});
   })
   .catch((err) => {
     return(err);
   });
 });
+
+
+/*
+################################################
+                SERVEEEEEEEEEEER
+################################################
+*/
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
