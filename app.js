9c1778cecd08a72d8dc6db75aca1d32eb8a550fd
ShantanuBhatia <sb5455@nyu.edu>
2019-10-02 19:59:59 +0530

    Heroku deployment housekeeping

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.post('/getarticle', (req, res) => {
 */
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
 });
